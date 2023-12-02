2a2c52fbd13205f306fba6218e9ad73beb4c69d8
doodleoo <doodleoocrypto@gmail.com>
2023-12-02 16:42:04 +0300

    test

--- src/widgets/footer/ui/gamePageFooter/GamePageFooter.tsx
+++ src/widgets/footer/ui/gamePageFooter/GamePageFooter.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import styles from "./GamePageFooter.module.scss"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faFlag, faComment, faArrowAltCircleLeft, faArrowAltCircleRight} from "@fortawesome/free-regular-svg-icons";
+import {useTelegram} from "../../../../shared/lib/hooks/useTelegram";
 
 const GamePageFooter = () => {
+    const {tg} = useTelegram()
+
     return (
-        <div className={styles.footer}>
+        <div className={styles.footer} style={tg.colorScheme === "light" ? {backgroundImage: `url("/assets/images/arcadum-bg-light.png")`} : {backgroundImage: `url("/assets/images/arcadum-bg-dark.png")`}}>
             <div className={styles.button__wrapper}>
                 <FontAwesomeIcon icon={faFlag} />
             </div>
