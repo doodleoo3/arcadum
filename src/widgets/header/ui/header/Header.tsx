5541a29a9d2b6979b36606d1bc16b875896d0d7c
doodleoo <doodleoocrypto@gmail.com>
2023-12-02 16:58:58 +0300

    test

--- src/widgets/header/ui/header/Header.tsx
+++ src/widgets/header/ui/header/Header.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from "./Header.module.scss"
+import {useTelegram} from "../../../../shared/lib/hooks/useTelegram";
 const Header = () => {
+    const {tg} = useTelegram()
+    
     return (
         <div className={styles.header}>
-            <div className={styles.logo}>
-                <h1>LOGO</h1>
-            </div>
+            <img className={styles.logo} src={tg.colorScheme === "light" ? "/assets/images/logo-white.png" : "/assets/images/logo-dark.png"} alt=""/>
+            <h1>ARCADUM</h1>
         </div>
     );
 };
