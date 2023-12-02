a7aeeab4a3563cc0e9ee2821d38d6410a9fa7ed4
doodleoo <doodleoocrypto@gmail.com>
2023-12-02 18:01:05 +0300

    test

--- src/entities/pageContainerItem/PageContainerItem.tsx
+++ src/entities/pageContainerItem/PageContainerItem.tsx
@@ -2,7 +2,7 @@ import React, {FC, PropsWithChildren} from 'react';
 import styles from "./PageContainerItem.module.scss"
 const PageContainerItem:FC<PropsWithChildren> = ({children}) => {
     return (
-        <div className={styles.item}>
+        <div className={styles.item} style={{backgroundImage: 'url("/assets/images/blockbg.png")'}}>
             {children}
         </div>
     );
