e9e6dfdc8d89f442f431dc3dd468da7147c88517
Platon Bulychev <nicoyazawatop@gmail.com>
2023-12-03 13:00:02 +0300

    fix: my turn

--- src/widgets/game/board/lib/context/GameReducer.ts
+++ src/widgets/game/board/lib/context/GameReducer.ts
@@ -1,57 +1,57 @@
-import { GameState, GameAction, types } from "./GameTypes";
+import { GameState, GameAction, types } from './GameTypes';
 
 const getPositions = (moves: string[]): string[] => {
-  return moves.map((move) => {
-    const n = move.length;
-    return move.substring(n - 2);
-  });
+    return moves.map((move) => {
+        const n = move.length;
+        return move.substring(n - 2);
+    });
 };
 
 const GameReducer = (state: GameState, action: GameAction): GameState => {
-  switch (action.type) {
-    case types.SET_POSSIBLE_MOVES:
-      return {
-        ...state,
-        possibleMoves: getPositions(action.moves),
-      };
-    case types.CLEAR_POSSIBLE_MOVES:
-      return {
-        ...state,
-        possibleMoves: [],
-      };
-    case types.SET_TURN:
-      return {
-        ...state,
-        turn: action.player,
-        check: action.check,
-      };
-    case types.GAME_OVER:
-      return {
-        ...state,
-        gameOver: true,
-        status: action.status,
-        turn: action.player,
-      };
-    case types.SET_PLAYER:
-      return { ...state, playerName: action.name };
-    case types.SET_PLAYER_COLOR:
-      return { ...state, playerColor: action.color };
-    case types.SET_OPPONENT:
-      return { ...state, opponentName: action.name };
-    case types.SET_MESSAGE:
-      return { ...state, message: action.message };
-    case types.CLEAR_MESSAGE:
-      return { ...state, message: "" };
-    case types.SET_OPPONENT_MOVES:
-      return { ...state, opponentMoves: action.moves };
-    case types.CLEAR_OPPONENT_MOVES:
-      return { ...state, opponentMoves: [] };
-    case types.SET_MY_TURN:
-      return { ...state, turn: action.player };
+    switch (action.type) {
+        case types.SET_POSSIBLE_MOVES:
+            return {
+                ...state,
+                possibleMoves: getPositions(action.moves),
+            };
+        case types.CLEAR_POSSIBLE_MOVES:
+            return {
+                ...state,
+                possibleMoves: [],
+            };
+        case types.SET_TURN:
+            return {
+                ...state,
+                turn: action.player,
+                check: action.check,
+            };
+        case types.GAME_OVER:
+            return {
+                ...state,
+                gameOver: true,
+                status: action.status,
+                turn: action.player,
+            };
+        case types.SET_PLAYER:
+            return { ...state, playerName: action.name };
+        case types.SET_PLAYER_COLOR:
+            return { ...state, playerColor: action.color };
+        case types.SET_OPPONENT:
+            return { ...state, opponentName: action.name };
+        case types.SET_MESSAGE:
+            return { ...state, message: action.message };
+        case types.CLEAR_MESSAGE:
+            return { ...state, message: '' };
+        case types.SET_OPPONENT_MOVES:
+            return { ...state, opponentMoves: action.moves };
+        case types.CLEAR_OPPONENT_MOVES:
+            return { ...state, opponentMoves: [] };
+        case types.SET_MY_TURN:
+            return { ...state, myTurn: action.player };
 
-    default:
-      return state;
-  }
+        default:
+            return state;
+    }
 };
 
 export default GameReducer;
